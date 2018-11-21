38548f08a2f6665ef30d3d4cd8a92d16f63c093a
sjm106 <simon.mills@lic.co.nz>
2018-11-21 13:06:46 +1300

    Add all the things

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,69 +1,101 @@
 import * as React from "react";
 import { render as ReactDomRender } from "react-dom";
 import * as openSocket from "socket.io-client";
+import "./style.css";
 
 class App extends React.Component<
   any,
-  { history: string[]; currentTextInput: string }
+  { history: string[]; currentTextInput: string; userName: string }
 > {
   private socket: SocketIOClient.Socket;
   constructor(props) {
     super(props);
     this.state = {
+      userName: "",
       history: [],
       currentTextInput: ""
     };
   }
+  chatWindow: HTMLDivElement;
 
-  handleChatReceipt = (newChat: string) => {
+  addChatToHistory = (newChat: string) => {
     const addedToHistory = [...this.state.history, newChat];
     this.setState({
       history: addedToHistory
     });
   };
 
-  handleBulkChatReceipt = (newChat: string[]) => {
-    const addedToHistory = [...this.state.history, ...newChat];
+  initChatWithHistory = (newChat: string[]) => {
+    const addedToHistory = [...newChat];
     this.setState({
       history: addedToHistory
     });
   };
 
+  handleUserConnection = userDetails => {
+    this.addChatToHistory(
+      `User ${userDetails.userId} joined at ${userDetails.connectedAt}`
+    );
+  };
+
+  handleUserDetails = userDetails => {
+    this.setState({
+      userName: userDetails.userId
+    });
+  };
+
   componentDidMount() {
     this.socket = openSocket("http://localhost:3001");
-    this.socket.on("handleChat", this.handleChatReceipt);
-    this.socket.on("handleChatHistory", this.handleBulkChatReceipt);
+    this.socket.on("handleChat", this.addChatToHistory);
+    this.socket.on("handleChatHistory", this.initChatWithHistory);
+    this.socket.on("broadcastUserConnection", this.handleUserConnection);
+    this.socket.on("broadcastUserDisconnection", this.addChatToHistory);
+    this.socket.on("sendUserDetails", this.handleUserDetails);
   }
 
   componentWillUnmount() {
     this.socket.close();
   }
 
-  chatSend = (event: React.FormEvent<HTMLFormElement>) => {
+  handlechatSend = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    this.chatSend();
+    this.scrollChatWindowToBottom();
+  };
+
+  chatSend = () => {
     this.socket.emit("submitChat", this.state.currentTextInput);
     this.setState({
       currentTextInput: ""
     });
   };
+
+  scrollChatWindowToBottom = () =>
+    this.chatWindow.scrollTo(0, this.chatWindow.clientHeight + 10);
+
   render() {
     return (
-      <div>
-        <p>I am the main component</p>
-        <div>
+      <div className="main">
+        <p>You are logged in as {this.state.userName}</p>
+        <div className="chat-window" ref={node => (this.chatWindow = node)}>
           <ul>
             {this.state.history.map((s, i) => (
               <li key={`${i}`}>{s}</li>
             ))}
           </ul>
         </div>
-        <form onSubmit={this.chatSend}>
+        <form
+          className="chat-input-form"
+          onSubmit={this.handlechatSend}
+          autoComplete="off"
+        >
           <input
             name="chat"
             type="text"
             placeholder="input text here"
             onChange={e => this.setState({ currentTextInput: e.target.value })}
             value={this.state.currentTextInput}
+            autoComplete="off"
           />
           <input type="submit" value="Submit" />
         </form>
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,10 @@ import * as http from "http";
 import * as express from "express";
 import * as socketIo from "socket.io";
 import * as cors from "cors";
+
+import { nouns } from "./nouns";
+import { adjectives } from "./adjectives";
+
 const server = express();
 server.use(cors());
 server.use(express.json());
@@ -9,14 +13,51 @@ const httpServer = http.createServer(server);
 const io = socketIo(httpServer, { origins: "*:*" });
 
 const chatHistory = [];
+const userDetails = {};
+
+const onUserConnect = (client: socketIo.Socket) => {
+  userDetails[client.id] = {
+    userId: generateUserName(),
+    connectedAt: new Date()
+  };
+  console.log("Connected with client id:", userDetails[client.id].userId);
+  io.emit("broadcastUserConnection", userDetails[client.id]);
+  assignUserName(client);
+  return onUserDisconnect(client.id);
+};
+
+const onUserDisconnect = (clientId: string) => () => {
+  const connectedAt = userDetails[clientId].connectedAt as Date;
+  const time = (Date.now().valueOf() - connectedAt.valueOf()) / 1000;
+  const message = `User ${
+    userDetails[clientId].userId
+  } disconnected after ${time} seconds.`;
+  console.log(message);
+  io.emit("broadcastUserDisconnection", message);
+};
+
+const onChatSubmission = (chat: string) => {
+  if (chat === "") return;
+  chatHistory.push(chat);
+  io.emit("handleChatHistory", chatHistory);
+};
+
+const generateUserName = () => {
+  const randomAdjective =
+    adjectives[Math.round(Math.random() * adjectives.length)];
+  const randomNoun = nouns[Math.round(Math.random() * nouns.length)];
+  return `${randomAdjective}-${randomNoun}`;
+};
+
+const assignUserName = (client: socketIo.Socket) => {
+  client.emit("sendUserDetails", userDetails[client.id]);
+};
 
 io.on("connection", client => {
-  client.on("submitChat", chat => {
-    chatHistory.push(chat);
-    client.emit("handleChat", chat);
-    console.log(chat);
-  });
+  const onDisconnect = onUserConnect(client);
   client.emit("handleChatHistory", chatHistory);
+  client.on("disconnect", onDisconnect);
+  client.on("submitChat", onChatSubmission);
 });
 
 io.listen(3001);
